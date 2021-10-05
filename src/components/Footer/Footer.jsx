1263095d06e857797c4805926a36e126d9112d36
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-04 20:10:32 -0400

    Created Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = (props) => {
+    return ( 
+            <div className="row">
+                <div className="Footer">
+                    <footer>
+                        <h4>My React Book Library by: N. Hawkins</h4>
+                    </footer>
+                </div>
+            </div>
+     );
+}
+ 
+export default Footer;
